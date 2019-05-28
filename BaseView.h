1e838aef0e18ac31dae67a5ddec8d444f9b6d08e
fzw_jishubu@163.com <fzw_jishubu@163.com>
2019-05-28 15:36:14 +0800

    my test first commit

---
+++ BaseView.h
@@ -0,0 +1,13 @@
+//
+//  BaseView.h
+//  CheckSupervise
+//
+//  Created by Jie on 2017/3/9.
+//  Copyright © 2017年 Geddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseView : UIView
+
+@end
---
+++ BaseView.m
@@ -0,0 +1,21 @@
+//
+//  BaseView.m
+//  CheckSupervise
+//
+//  Created by Jie on 2017/3/9.
+//  Copyright © 2017年 Geddy. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@implementation BaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
